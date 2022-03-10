968b63c715bf6eed24207001a0a23b4c84d62a58
Mariangel <mariangel.yajure@gmail.com>
2022-03-10 16:47:36 -0430

    Listas y bucles (utilizando map para recorrer el array)

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,33 @@
 import React from "react"
 
 function Dashboard(){
+
+	const notes = [
+		{id: 1, title: 'nota1', body: 'lorem ipsum'},
+		{id: 2, title: 'nota2', body: 'lorem ipsum'},
+		{id: 3, title: 'nota3', body: 'lorem ipsum'},
+		{id: 4, title: 'nota4', body: 'lorem ipsum'},
+		{id: 5, title: 'nota5', body: 'lorem ipsum'}
+	];
+
 	return (
-		<h1>Hola mundo desde componente APP</h1>
-		)
+	
+		<div>
+			
+			<h1>Listado de notas</h1>
+
+			<ul>
+			{
+				//Recorriendo el array con map
+				notes.map(note => {
+					return <li>ID: {note.id} titulo: {note.title} Cuerpo: {note.body}</li>;
+				})
+			}
+			</ul>
+			
+		</div>
+
+	)
 }
 
 export default Dashboard;
