09832e08543fbc622cb5ce885b6af1d9b7bb2928
Mariangel <mariangel.yajure@gmail.com>
2022-03-11 18:45:23 -0430

    Usando estados en react

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,22 @@
 import React from "react"
+import {useState} from "react"
 
 function Dashboard(){
 
-	const notes = [
+	const [notes, setNotes] = useState([
 		{id: 1, title: 'nota1', body: 'lorem ipsum'},
 		{id: 2, title: 'nota2', body: 'lorem ipsum'},
 		{id: 3, title: 'nota3', body: 'lorem ipsum'},
 		{id: 4, title: 'nota4', body: 'lorem ipsum'},
 		{id: 5, title: 'nota5', body: 'lorem ipsum'}
-	];
+	]);
+
+	const changeState = () => {
+		
+		const reg = {id: 6, title: 'nota6', body: 'lorem ipsum'};
+		
+		setNotes(notes.concat(reg)); //Para agregar el registro al que ya se tenía. Y set para guardarlo.
+	}
 
 	return (
 	
@@ -24,7 +32,7 @@ function Dashboard(){
 				})
 			}
 			</ul>
-			
+			<button onClick={changeState}>Cambiar estado</button>
 		</div>
 
 	)
