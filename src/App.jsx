1a343b4e312308fe43002719d1883ed9108f2742
Mariangel <mariangel.yajure@gmail.com>
2022-03-11 21:27:40 -0430

    Formulario y agregando elementos

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,13 @@ import {useState} from "react"
 
 function Dashboard(){
 
+	const initialNotes = {
+		id: '', 
+		title: '', 
+		body: ''
+	}
+	const [note, setNote] = useState(initialNotes); //Guardaran los datos en initialNotes
+
 	const [notes, setNotes] = useState([
 		{id: 1, title: 'nota1', body: 'lorem ipsum'},
 		{id: 2, title: 'nota2', body: 'lorem ipsum'},
@@ -11,12 +18,33 @@ function Dashboard(){
 		{id: 5, title: 'nota5', body: 'lorem ipsum'}
 	]);
 
-	const changeState = () => {
-		
-		const reg = {id: 6, title: 'nota6', body: 'lorem ipsum'};
-		
-		setNotes(notes.concat(reg)); //Para agregar el registro al que ya se tenía. Y set para guardarlo.
+
+	const addNote = (e) => {
+
+		e.preventDefault();
+
+		if(note.title === "" || note.body === ""){return}
+
+		setNotes([
+			...notes, //para obtener todas las notas
+				{
+					...note, //para obtener lo ya escrito
+					id: Math.max(...notes.map(note => note.id)) + 1
+				}
+			])
+
+		console.log(Math.max(...notes.map(note => note.id)) + 1); //Para que nos de el número mayor y ... para que solo devuelva numeros.
+
+		// note.map(note => note.id);
+
 	}
+
+	// const changeState = () => {
+		
+	// 	const reg = {id: 6, title: 'nota6', body: 'lorem ipsum'};
+		
+	// 	setNotes(notes.concat(reg)); //Para agregar el registro al que ya se tenía. Y set para guardarlo.
+	// }
 
 	return (
 	
@@ -28,11 +56,24 @@ function Dashboard(){
 			{
 				//Recorriendo el array con map
 				notes.map(note => {
-					return <li>ID: {note.id} titulo: {note.title} Cuerpo: {note.body}</li>;
+					return <li key={note.id}>ID: {note.id} titulo: {note.title} Cuerpo: {note.body}</li>;
 				})
 			}
 			</ul>
-			<button onClick={changeState}>Cambiar estado</button>
+
+			<form onSubmit={addNote} >
+				<label htmlFor="title">
+					Título
+					<input id="title" type="text" onChange={(e) => setNote({...note,title: e.target.value})} />
+				</label>
+				<br/>
+				<label htmlFor="body">
+					Cuerpo
+					<input id="body" type="text" onChange={(e) => setNote({...note,body: e.target.value})} />
+				</label>
+				<br/>
+				<button>Agregar</button>
+			</form>
 		</div>
 
 	)
