a058e7d2e144f35da4b0f0e2b7bf10e75feccf71
Mariangel <mariangel.yajure@gmail.com>
2022-03-18 11:42:09 -0430

    Renderizado condicional

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,11 +1,51 @@
 import React from 'react'
+import {useState} from 'react'
 
 const Note = ({note, deleteNote}) => {
+
+	const [modeEdit, setModeEdit] = useState(false);
+	const [item, setItem] = useState(note);
+
+	const toggle = () => {
+		setModeEdit(!modeEdit);
+		setItem(note);
+	}
+
 	return (
 		<li style={{marginBottom: '.6em'}}>
-			ID: {note.id} titulo: {note.title} Cuerpo: {note.body}
-			//Otra manera de llamar una función directamente para pasar parámetros
-			<button onClick={() => deleteNote(note.id)}>x</button> 
+			<div>ID: {note.id}</div>
+			{
+				modeEdit ? 
+
+				<label>
+					Titulo: 
+					<input type="text" value={item.title} onChange={(e) => setItem({...item, title: e.target.value})}/>
+				</label>
+				: <div>titulo: {note.title}</div>
+			}
+			{
+				modeEdit ? 
+
+				<label>
+					Cuerpo: 
+					<input type="text" value={item.body} onChange={(e) => setItem({...item, body: e.target.value})}/>
+				</label>
+				: <div>Cuerpo: {note.body}</div>
+			}
+			
+			{
+				modeEdit &&
+				<button>Guardar</button>
+			}
+			
+			<button onClick={() => toggle()}>{modeEdit ? 'Cancelar' : 'Editar'}</button> 
+
+			{
+				!modeEdit &&
+				<button onClick={() => deleteNote(note.id)}>x</button> 
+			}
+
+			
 		</li>
 	);
 }
